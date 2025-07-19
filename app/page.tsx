fceb96fe5ee0c63b70aa7e7a1784d6c80e3fdbc4
studio_bibu <studio.bibu@gmail.com>
2025-07-19 18:03:56 +0900

    719-2変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
 
   return (
+    <section className={styles.top}>
     <div>
       <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
       <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
     </div>
+   <Image
+    className={styles.bgimg}
+    src="/img-mv.jpg"
+    alt=""
+    width={4000}
+    height={1200}
+    />
+    </section>
   );
 }
